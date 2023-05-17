16a931bd2595c94e2870507013c3aa965e898e28
ericlogut <ericlogut@gmail.com>
2023-05-17 14:01:51 +0200

    Implementació de nova funció

---
+++ CalculaFactorial.java
@@ -0,0 +1,9 @@
+public class CalculaFactorial {
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
