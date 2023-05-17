6212cde8679c878f67010987e4814197f74efa74
ericlogut <ericlogut@gmail.com>
2023-05-17 13:05:28 +0200

    Canvis en el clon

--- CalculadoraMitjana.java
+++ CalculadoraMitjana.java
@@ -2,11 +2,7 @@ import java.util.List;
 
 public class CalculadoraMitjana {
 
-    public double calcularMitjana(List<Double> numeros) {
-        if (numeros == null || numeros.isEmpty()) {
-            throw new IllegalArgumentException("La llista no pot estar buida.");
-        }
-    
+    public double calcularMitjana(List<Double> numeros) {    
         double suma = 0.0;
         int n = numeros.size();
         for (double num : numeros) {
